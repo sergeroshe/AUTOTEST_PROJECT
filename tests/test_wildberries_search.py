2ce811eae55d374bb7b46c590ed7ec5beebb80f1
sergeroshe <62591932+sergeroshe@users.noreply.github.com>
2023-10-25 20:50:47 +0300

    Initial commit_

--- tests/test_wildberries_search.py
+++ tests/test_wildberries_search.py
@@ -1,21 +1,18 @@
 from pages import Main_page, ProductsForWomen, WomenOuterwear
 
 
-def test_is_search_result_relevant(wildberries_main_page, driver):
-    wildberries_main_page: Main_page
+def test_is_search_result_relevant(wildberries_main_page: Main_page):
     wildberries_main_page.check_search_results()
     assert wildberries_main_page.both_words_present()
 
 
-def test_product_menu(women_outerwear_page, driver):
-    women_outerwear_page: ProductsForWomen
+def test_product_menu(women_outerwear_page: ProductsForWomen):
     women_outerwear_page.open_women_outerwear_page()
     assert women_outerwear_page.is_page_loaded()
 
 
-def test_search_jackets(jackets_search, driver):
+def test_search_jackets(jackets_search: WomenOuterwear):
     searched_item = 'Куртка'
-    jackets_search: WomenOuterwear
     jackets_search.go_to_site()
     outerwear_page_content = jackets_search.get_outerwear_page_content()
     assert searched_item in outerwear_page_content
